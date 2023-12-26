f44f4a77df92f017b65761dfa6705529bc3301cb
Kristoff Malejczuk <kamalejc@uwaterloo.ca>
2023-12-26 14:12:33 -0500

    control KeyRepeat speed and key PressAndHold behaviour

---
+++ keyDefaults.sh
@@ -0,0 +1,3 @@
+defaults write -g InitialKeyRepeat -int 10 # default min setting is 15 (225ms)
+defaults write -g KeyRepeat -int 1 # default min setting is 2 (30ms)
+defaults write -g ApplePressAndHoldEnabled -bool false # default is true
